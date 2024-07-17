8093135f8224b1f509e8ec914caa1ad7a51947a6
Heeji Hyun <wlgmlgus@gmail.com>
2024-07-17 15:45:09 -0400

    commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const selectBreed = document.querySelector('#selectBreed')
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    fetch('https://api.thecatapi.com/v1/breeds')
+   .then(res => res.json())
+   .then(catArray => {
+    catObjects = catArray
+
+    catArray.forEach(catObject => {
+        const catOPtion = document.createElement("option")
+        catOPtion.value = catObject.id
+        catOPtion.innerText = catObject.name
+
+        // console.log(catObject)
+        selectBreed.append(catOPtion)
+
+    }) 
+   })
+   .catch(err => console.log(err))
+})
+
+
+selectBreed.addEventListener("change", (e) => {
+    e.preventDefault()
+
+    const found = catObjects.find(catObj => e.target.value === catObj.id)
+    console.log(found)
+
+    const card = document.createElement("div")
+    card.className = 'card'
+    card.innerHTML = `
+    <div class="card-img">
+    <img src = https://cdn2.thecatapi.com/images/${found.reference_image_id}.jpg>
+    </div>
+    <h3 id="cat-breed">${found.name}</h3>
+    <li id="description"><b>Description:</b> ${found.description} </li>
+    <li id="temperament"><b>Temperament:</b> ${found.temperament} </li>
+    <li id="lifespan"><b>Life_span:</b> ${found.lifespan} </li>
+    <li id="origin"><b>Origin:</b> ${found.origin} </li>
+    `
+    const cardCollection = document.getElementById("breed-collection")
+    cardCollection.append(card)
+
+    const deleteBtn = document.createElement("button")
+    deleteBtn.addEventListener('click', () => {
+        card.remove()
+    })
+
+    deleteBtn.innerText = 'Delete'
+    card.append(deleteBtn)
+})
+
